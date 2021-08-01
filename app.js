af8fbc2aa84ae72fbdcfd34f89ee77e8365a0388
relieyanhilman <relieyanhilman93@gmail.com>
2021-08-01 12:46:09 +0800

    some changes on port and how to run

--- app.js
+++ app.js
@@ -131,7 +131,7 @@ app.use((err, req, res, next) => {
 })
 
 
-
-app.listen(3000, () => {
-  console.log('server sedang berjalan di port 3000')
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`server sedang berjalan di port ${port}`)
 })
